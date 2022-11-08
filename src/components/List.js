088b1758f4b91b63c64b50423ae97f24245c396e
Biswajit Deb Barman <108471872+Biswajit-Mercuri@users.noreply.github.com>
2022-11-08 21:18:59 +0530

    push

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Item from './Item';
+import { useSelector } from 'react-redux';
+
+const List = () => {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
